e6397dfb7abb355f43de4c9814d6ae6bce2a2683
Guido Leeuwenstein <guidoleeuw@gmail.com>
2024-08-11 16:08:04 +0200

    Moved libs to dir and started implementing velocity

--- src/main.c
+++ src/main.c
@@ -1,17 +1,43 @@
-#include "../raylib.h"
+#include "../lib/raylib.h"
+#include <stdlib.h>
 
-#define WIDTH 1920
-#define HIGHT 1080
+const int WIDTH = 1920;
+const int HIGHT = 1080;
+const int GRAV = 9;
+
+typedef struct {
+    int dir;
+    int vel;
+  } vec2 ;
+
+
+typedef struct {
+  int posx;
+  int posy;
+  vec2 enertia;
+  } Circle;
+
+void updatePos(Circle* circle) {
+  circle->enertia.vel = circle->enertia.vel * GRAV;
+  //TODO: left here
+  circle->posy = circle->posy; 
+}
 
 int main() {
   InitWindow(WIDTH, HIGHT, "This some bullshit");
   SetTargetFPS(60);
-  while(!WindowShouldClose()) {
-
-  BeginDrawing();
   
-  DrawCircle( WIDTH/2, HIGHT/2, 100, RED);
+  Circle * circle = malloc(sizeof(Circle));
+  circle->posx = WIDTH/2;
+  circle->posy = HIGHT/2;
+  circle->enertia.vel = 1;
+  circle->enertia.dir = 0;
 
-  EndDrawing();
+  while(!WindowShouldClose()) {
+    
+
+    BeginDrawing();
+    DrawCircle( WIDTH/2, HIGHT/2, 100, RED);
+    EndDrawing();
   }
 }
